c1ce918709f491cce1cf5fbdcedc32a62fed95b8
MaruyamaNao <61174627+MaruyamaNao@users.noreply.github.com>
2020-04-01 10:02:07 +0900

    投稿機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,4 +8,15 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
   end
 
+  def create
+    Tweet.create(tweet_params)
+    redirect_to root_path
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:text, :image)
+  end
+
+
 end
